18d09c93c1f6d82f7575385ad20073d3002c717f
Gerard Sans <gsans@amazon.com>
2019-06-23 16:43:22 +0300

    Add code and styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Storage } from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react'
+
+async function addToStorage() {
+  await Storage.put('javascript/MyReactComponent.js', `
+    import React from 'react'
+    const App = () => (
+      <p>Hello World</p>
+    )
+    export default App
+  `)
+  console.log('Code stored in S3!')
+}
+
+async function addToStorageHelloWorld() {
+  await Storage.put(' helloworld.txt', 'Hello World')
+  console.log('HelloWorld stored in S3!')
+}
+
+async function readFolderFromStorage(folder) {
+  const data = await Storage.list(folder); // 'javascript'
+  console.log('Folder: ', data);
+}
+
+async function readFileFromStorage(file) {
+  const data = await Storage.get(file) // 'javascript/MyReactComponent.js'
+  console.log('File: ', data)
+}
+
+async function readRootFromStorage() {
+  const data = await Storage.list('')
+  console.log('Root: ', data)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app-header">
+          <div className="app-logo">
+              <img src="https://aws-amplify.github.io/images/Logos/Amplify-Logo-White.svg" alt="AWS Amplify" />
+          </div>
+          <h1>Welcome to the Amplify Framework</h1>
+      </div>
+      <div className="app-body">
+        <div>
+          <button onClick={addToStorage}>Add Code To Storage</button>
+          <button onClick={addToStorageHelloWorld}>Add Txt To Storage</button>
+        </div>
+        <div>
+          <button onClick={readFolderFromStorage.bind(this, 'javascript')}>Read Folder</button>
+          <button onClick={readFileFromStorage.bind(this, 'javascript/MyReactComponent.js')}>Read File</button>
+          <button onClick={readRootFromStorage}>Read Root</button>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true })
